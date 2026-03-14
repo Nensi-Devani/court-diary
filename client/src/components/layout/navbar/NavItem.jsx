e73e6c6c811fc1562d69707340e378f5bce706fb
Nensi Devani <ndevani894@rku.ac.in>
2026-03-15 01:31:08 +0530

    modify code for active tab

--- client/src/components/layout/navbar/NavItem.jsx
+++ client/src/components/layout/navbar/NavItem.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-const NavItem = ({ to, icon : Icon, text }) => {
+const NavItem = ({ to, icon: Icon, text }) => {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
   return (
     <li className='nav-item'>
-      <Link to={to} className='nav-link'>
-        <Icon className={'nav-icon'} />
-        <p className='mx-1'>{text}</p>
+      <Link to={to} className={`nav-link ${isActive ? 'active' : ''}`}>
+        <Icon className='nav-icon' />
+        <p>{text}</p>
       </Link>
     </li>
   )
