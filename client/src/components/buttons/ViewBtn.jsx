1ea3c3915c9dd2443f52bc54a67185f8fc502190
Nensi Devani <ndevani894@rku.ac.in>
2026-03-31 01:12:23 +0530

    add link

--- client/src/components/buttons/ViewBtn.jsx
+++ client/src/components/buttons/ViewBtn.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { FaEye } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
-const ViewBtn = ({ onClick }) => {
+const ViewBtn = ({ link }) => {
+  const navigate = useNavigate();
   return (
-    <button className='btn btn-primary btn-sm' onClick={onClick}>
+    <button 
+      className='btn btn-primary btn-sm' 
+      onClick={() => navigate(link)}
+    >
       <FaEye />
     </button>
   )
