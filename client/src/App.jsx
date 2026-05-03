b6b28a3109df924b2a55e01333ecc23b0e6919b0
Nensi Devani <ndevani894@rku.ac.in>
2026-05-04 01:20:14 +0530

    updated app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import AppRoutes from './routes/AppRoutes'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'dummy-client-id.apps.googleusercontent.com';
+
   return (
-    <>
+    <GoogleOAuthProvider clientId={clientId}>
+      <ToastContainer position="top-right" />
       <AppRoutes />
-    </>
+    </GoogleOAuthProvider>
   )
 }
 
